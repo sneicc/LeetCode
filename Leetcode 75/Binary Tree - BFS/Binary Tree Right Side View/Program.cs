608a7528f4396ab3c9a31a685705fde63705d5c3
Andrey <102552777+sneicc@users.noreply.github.com>
2024-03-07 21:58:19 +0300

    Binary Tree BFS topic

---
+++ Leetcode 75/Binary Tree - BFS/Binary Tree Right Side View/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Binary_Tree_Right_Side_View
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public IList<int> RightSideView(TreeNode root)
+        {
+            var levels = new List<Stack<int>>();
+            var ans = new List<int>();
+
+            solve(root, 0, levels);
+
+            foreach(var level in levels)
+            {
+                ans.Add(level.Pop());
+            }
+
+            return ans;
+        }
+
+        public void solve(TreeNode root, int level, List<Stack<int>> levels)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            if (level >= levels.Count)
+            {
+                levels.Add(new Stack<int>());
+            }
+
+            levels[level].Push(root.val);
+
+            level++;
+            solve(root.left, level, levels);
+            solve(root.right, level, levels);
+        }
+    }
+}
---
+++ Leetcode 75/Binary Tree - BFS/Maximum Level Sum of a Binary Tree/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Maximum_Level_Sum_of_a_Binary_Tree
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode
+            {
+                val = -100,
+                left = new TreeNode
+                {
+                    val = -200,
+                    left = new TreeNode
+                    {
+                        val = -20,
+                        left = null,
+                        right = null
+                    },
+                    right = new TreeNode
+                    {
+                        val = -5,
+                        left = null,
+                        right = null
+                    }
+                },
+                right = new TreeNode
+                {
+                    val = -300,
+                    left = new TreeNode
+                    {
+                        val = -10,
+                        left = null,
+                        right = null
+                    },
+                    right = null
+                }
+            };
+
+            Console.WriteLine(MaxLevelSum(root));
+
+            Console.ReadLine();
+
+        }
+
+        public static int MaxLevelSum(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            int ans = 1;
+            int level = 1;
+            int max = Int32.MinValue;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                int acc = 0;
+                int count = queue.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    var curr = queue.Dequeue();
+
+                    if (curr.left is not null)
+                    {
+                        queue.Enqueue(curr.left);
+                    }
+                    if(curr.right is not null)
+                    {
+                        queue.Enqueue(curr.right);
+                    }                   
+
+                    acc += curr.val;
+                }
+
+                if(acc > max)
+                {
+                    max = acc;
+                    ans = level;
+                }
+                level++;
+            }
+
+            return ans;
+        }
+    }
+}
