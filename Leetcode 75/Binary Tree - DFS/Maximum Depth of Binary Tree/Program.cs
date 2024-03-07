fab7aee547d77037097982649d2169a6ae6be3c7
Andrey <102552777+sneicc@users.noreply.github.com>
2024-03-07 21:45:32 +0300

    Binary Tree DFS

---
+++ Leetcode 75/Binary Tree - DFS/Maximum Depth of Binary Tree/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Maximum_Depth_of_Binary_Tree
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            int depth = 1;
+            int max = 1;
+
+            EnterNode(root, ref depth, ref max);
+
+            return max;
+        }
+
+        public void EnterNode(TreeNode node, ref int currentDepth, ref int maxDepth)
+        {
+            maxDepth = Math.Max(maxDepth, currentDepth);
+
+            if (node.left != null)
+            {
+                currentDepth++;
+                EnterNode(node.left, ref currentDepth, ref maxDepth);
+                currentDepth--;
+            }
+
+            if(node.right != null)
+            {
+                currentDepth++;
+                EnterNode(node.right, ref currentDepth, ref maxDepth);
+                currentDepth--;
+            }
+        }
+    }
+}
