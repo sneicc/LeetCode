b2b00ea0a2d1f5ddddeab48ddc7ee588f5262130
Andrey <102552777+sneicc@users.noreply.github.com>
2024-03-09 01:39:44 +0300

    Binary Search Tree topic

---
+++ Leetcode 75/Binary Search Tree/Delete Node in a BST/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Delete_Node_in_a_BST
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var t = new TreeNode 
+            {
+                val = 5,
+                left = new TreeNode 
+                {
+                    val = 3,
+                    left = new TreeNode
+                    {
+                        val = 2,
+                    },
+                    right = new TreeNode
+                    {
+                        val = 4
+                    }
+                },
+            };
+
+            DeleteNode(t, 3);
+        }
+
+        public static TreeNode DeleteNode(TreeNode root, int key)
+        {
+            if(root is null)
+            {
+                return null;
+            }
+
+            if (key < root.val)
+            {
+                root.left = DeleteNode(root.left, key);
+            }
+            else if(key > root.val)
+            {
+                root.right = DeleteNode(root.right, key);
+            }
+            else
+            {
+                if(root.left is null)
+                {
+                    return root.right;
+                }
+                if(root.right is null)
+                {
+                    return root.left;
+                }
+
+                TreeNode node = root.right;
+                while (node.left != null)
+                {
+                    node = node.left;
+                }
+                root.val = node.val;
+                root.right = DeleteNode(root.right, node.val);
+            }           
+
+            return root;
+        }
+    }
+}
---
+++ Leetcode 75/Binary Search Tree/Search in a Binary Search Tree/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Search_in_a_Binary_Search_Tree
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            TreeNode current = root;
+            while (current != null)
+            {
+                if (val == current.val)
+                {
+                    return current;
+                }
+
+                current = val < current.val ? current.left : current.right;              
+            }
+
+            return null;
+        }
+    }
+}
