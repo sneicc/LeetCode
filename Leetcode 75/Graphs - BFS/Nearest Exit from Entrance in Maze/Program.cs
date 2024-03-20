940478c291ccfb9d14ba545996459839a895b742
Andrey <102552777+sneicc@users.noreply.github.com>
2024-03-20 20:55:41 +0300

    Graphs - BFS topic

---
+++ Leetcode 75/Graphs - BFS/Nearest Exit from Entrance in Maze/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Nearest_Exit_from_Entrance_in_Maze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NearestExit(([
+                ['.', '+']]), [0, 0]));
+            Console.ReadLine();
+        }
+
+        public static int NearestExit(char[][] maze, int[] entrance)
+        {
+            var dir = new int[] {0, 1, 0, -1, 0};
+            var queue = new Queue<(int, int, int)>();
+            queue.Enqueue((entrance[0], entrance[1], 0));
+
+            while(queue.Any())
+            {
+                var (i, j, dist) = queue.Dequeue();
+
+                if (i < 0 || i == maze.Length) continue;
+                if (j < 0 || j == maze[0].Length) continue;
+                if (maze[i][j] != '.') continue;
+
+                maze[i][j] = '_';
+
+                if (dist > 0 && (i == 0 || i == maze.Length - 1)) return dist;
+                if (dist > 0 && (j == 0 || j == maze[0].Length - 1)) return dist;
+
+                for (int k = 0; k < dir.Length - 1; k++)
+                {
+                    queue.Enqueue((i + dir[k], j + dir[k + 1], dist + 1));
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    /*
+    ['+','.','+','+','+','+','+'],
+    ['+','.','+','.','.','.','+'],
+    ['+','.','+','.','+','.','+'],
+    ['+','.','.','.','+','.','+'],
+    ['+','+','+','+','+','.','+']
+    
+    ['+','.','+','+','+','+','+'],
+    ['+','.','+','.','.','.','+'],
+    ['+','.','+','.','+','.','+'],
+    ['+','.','.','.','.','.','+'],
+    ['+','+','+','+','.','+','.']
+    */
+}
---
+++ Leetcode 75/Graphs - BFS/Rotting Oranges/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Rotting_Oranges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OrangesRotting([[2, 1, 1], [1, 1, 0], [0, 1, 1]]);
+        }
+
+        public static int OrangesRotting(int[][] grid)
+        {
+            var direction = new int[] { 0, 1, 0, -1, 0 };
+            int minutes = 0;
+            int oranges = 0;
+
+            var queue = new Queue<(int, int)>();
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == 2)
+                    {
+                        for (int k = 0; k < direction.Length - 1; k++)
+                        {
+                            queue.Enqueue((i + direction[k], j + direction[k + 1]));
+                        }
+                    }
+                    if (grid[i][j] == 1) oranges++;
+                }
+            }
+
+            while(true)
+            {
+                int queueLen = queue.Count;
+                for (int l = 0; l < queueLen; l++)
+                {
+                    var (i, j) = queue.Dequeue();
+
+                    if (i < 0 || i >= grid.Length) continue;
+                    if (j < 0 || j >= grid[0].Length) continue;
+                    if (grid[i][j] != 1) continue;
+
+                    grid[i][j] = 2;
+                    oranges--;
+                    
+
+                    for (int k = 0; k < direction.Length - 1; k++)
+                    {
+                        queue.Enqueue((i + direction[k], j + direction[k + 1]));
+                    }
+                }
+
+                if (!queue.Any()) break;
+
+                minutes++;
+            }
+
+            return oranges == 0 ? minutes : -1;
+        }
+    }
+}
