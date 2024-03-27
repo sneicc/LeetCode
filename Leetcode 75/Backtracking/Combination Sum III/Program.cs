20151f2b532650a249bbe030281dde10ab6c9878
Andrey <102552777+sneicc@users.noreply.github.com>
2024-03-27 18:39:40 +0300

    Backtracking topic

---
+++ Leetcode 75/Backtracking/Combination Sum III/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Combination_Sum_III
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public IList<IList<int>> CombinationSum3(int k, int n)
+        {
+            var ans = new List<IList<int>>();
+            var linkedList = new LinkedList<int>();
+
+            CreateCombinations(0, 1, 0);
+
+            return ans;
+
+            void CreateCombinations(int depth, int firstValue, int sum)
+            {
+                if (sum > n) return;
+                if (depth >= k)
+                {
+                    if(sum == n) ans.Add(new List<int>(linkedList));
+                    return;
+                }
+
+                for (int i = firstValue; i < 10; i++)
+                {
+                    linkedList.AddLast(i);
+                    CreateCombinations(depth + 1, i + 1, sum + i);
+                    linkedList.RemoveLast();
+                }
+            }
+        }
+    }
+}
---
+++ Leetcode 75/Backtracking/Letter Combinations of a Phone Number/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Letter_Combinations_of_a_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public IList<string> LetterCombinations(string digits)
+        {         
+            int len = digits.Length;
+            var ans = new List<string>();
+            if (len == 0)
+            {
+                return ans;
+            }
+
+            var letters = new Dictionary<int, char[]>
+            {
+                { 2, ['a', 'b', 'c'] },
+                { 3, ['d', 'e', 'f'] },
+                { 4, ['g', 'h', 'i'] },
+                { 5, ['j', 'k', 'l'] },
+                { 6, ['m', 'n', 'o'] },
+                { 7, ['p', 'q', 'r', 's'] },
+                { 8, ['t', 'u', 'v'] },
+                { 9, ['w', 'x', 'y', 'z'] }
+            };
+
+            var digitsArr = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                digitsArr[i] = digits[i] - '0';
+            }
+
+            CreateCombinations();
+
+            return ans;
+
+            void CreateCombinations(string combination = "", int depth = 0)
+            {
+                if(depth >= len)
+                {
+                    ans.Add(combination);
+                    return;
+                }
+
+                char[] currentLetters = letters[digitsArr[depth]];
+                for (int i = 0; i < currentLetters.Length; i++)
+                {
+                    CreateCombinations(combination + currentLetters[i], depth + 1);
+                }
+            }
+        }
+    }
+}
