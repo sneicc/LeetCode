5a6e90b081c2231b077797155d1f30cc83ce3ac2
Andrey <102552777+sneicc@users.noreply.github.com>
2024-06-14 23:18:30 +0300

    Trie topic

---
+++ Leetcode 75/Trie/Implement Trie (Prefix Tree)/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Implement_Trie__Prefix_Tree_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Trie();
+            t.Insert("app");
+            t.Insert("apple");
+            t.Insert("beer");
+            t.Insert("add");
+            t.Insert("jam");
+            t.Insert("rental");
+
+
+            Console.WriteLine(t.Search("apps"));
+            Console.WriteLine(t.Search("app"));
+        }
+    }
+
+    public class Trie
+    {
+        public List<Trie> Child { get; private set; }
+        public bool IsTerminal { get; private set; }
+        public char Value { get; private set; }
+
+        public Trie()
+        {
+            IsTerminal = false;
+            Value = ' ';
+            Child = new List<Trie>();
+        }
+        
+        private Trie(bool  isTerminal, char value)
+        {
+            IsTerminal = isTerminal;
+            Value = value;
+            Child = new List<Trie>();
+        }
+
+        public void Insert(string word)
+        {
+            Insert(word, 0);
+        }
+        private void Insert(string word, int index)
+        {
+            char letter = word[index];
+            bool isLastLetter = index == word.Length - 1;
+
+            for (int i = 0; i < Child.Count; i++)
+            {
+                char childVal = Child[i].Value;
+                if (childVal == letter)
+                {
+                    if (!isLastLetter)
+                    {
+                        Child[i].Insert(word, index + 1);
+                    }
+                    else
+                    {
+                        Child.Add(new Trie(true, letter));
+                    }
+                    return;
+                }
+            }
+
+            Trie t;
+            if (isLastLetter)
+            {
+                t = new Trie(true, letter);
+            }
+            else
+            {
+                t = new Trie(false, letter);
+                t.Insert(word, index + 1);
+            }
+            Child.Add(t);
+        }
+
+        public bool Search(string word)
+        {
+            return Search(word, 0);
+        }
+        private bool Search(string word, int index)
+        {
+            bool ans = false;
+            bool isLastLetter = index == word.Length - 1;
+
+            for (int i = 0; i < Child.Count; i++)
+            {
+                if (Child[i].Value == word[index])
+                {
+                    if (!isLastLetter)
+                    {
+                        ans = Child[i].Search(word, index + 1);
+                        return ans;
+                    }
+                    else if (Child[i].IsTerminal)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return ans;
+        }
+
+        public bool StartsWith(string prefix)
+        {
+            return StartsWith(prefix, 0);
+        }
+        private bool StartsWith(string prefix, int index)
+        {
+            bool ans = false;
+            bool isLastLetter = index == prefix.Length - 1;
+
+            for (int i = 0; i < Child.Count; i++)
+            {
+                if (Child[i].Value == prefix[index])
+                {
+                    if (!isLastLetter)
+                    {
+                        ans = Child[i].StartsWith(prefix, index + 1);
+                        return ans;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return ans;
+        } 
+    }
+}
---
+++ Leetcode 75/Trie/Search Suggestions System/Program.cs
@@ -0,0 +1,112 @@
+﻿using System.Text;
+
+namespace Search_Suggestions_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SuggestedProducts(["mobile", "mouse", "moneypot", "monitor", "mousepad"], "mouse");
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static IList<IList<string>> SuggestedProducts(string[] products, string searchWord)
+        {
+            var trie = new Trie();
+            var ans = new List<IList<string>>();
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                trie.Insert(products[i]);
+            }
+
+            for (int i = 0; i < searchWord.Length; i++)
+            {
+                List<string> suggestions = trie.GetWordsWithPrefix(searchWord[..(i + 1)]);
+                suggestions.Sort();
+                
+                if(suggestions.Count > 3) suggestions.RemoveRange(3, suggestions.Count - 3);
+                ans.Add(suggestions);
+            }
+
+            return ans;
+        }
+    }
+
+    public class Trie
+    {
+        private TrieNode _root;
+
+        public Trie()
+        {
+            _root = new TrieNode();
+        }
+
+        public void Insert(string word)
+        {
+            var current = _root;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (!current.Childs.ContainsKey(word[i]))
+                {
+                    current.Childs.Add(word[i], new TrieNode());
+                }
+
+                current = current.Childs[word[i]];
+            }
+
+            current.IsTerminal = true;
+        }
+
+        public List<string> GetWordsWithPrefix(string prefix)
+        {
+            var sb = new StringBuilder(prefix);
+            var ans = new List<string>();
+
+            TrieNode curNode = _root;
+            for (int i = 0;i < prefix.Length; i++)
+            {
+                if (curNode.Childs.ContainsKey(prefix[i]))
+                {
+                    curNode = curNode.Childs[prefix[i]];
+                }
+                else
+                {
+                    return ans;
+                }
+            }
+
+            if(curNode.IsTerminal) ans.Add(sb.ToString());
+
+            DFS(curNode);
+
+            return ans;
+
+            void DFS(TrieNode node)
+            {
+                foreach (var child in node.Childs) 
+                {
+                    sb.Append(child.Key);
+                    if(child.Value.IsTerminal) ans.Add(sb.ToString());
+
+                    DFS(child.Value);
+
+                    sb.Remove(sb.Length - 1, 1);
+                }
+            }
+        }
+
+        private class TrieNode
+        {
+            public Dictionary<char, TrieNode> Childs;
+            public bool IsTerminal;
+
+            public TrieNode()
+            {
+                Childs = new Dictionary<char, TrieNode>();
+                IsTerminal = false;
+            }
+        }
+    }
+}
